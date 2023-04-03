db0954c270060523665cb94f1dd43a55563220ab
Filip G <filipgajko18@gmail.com>
2023-04-03 16:45:40 +0200

    Overriding RankedGradeBooks GetLetterGrade Method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,5 +12,45 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count > 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires a minimum of 5 students.");
+            }
+
+            int betterStudents = 0;
+
+            foreach (var student in Students)
+            {
+                if (averageGrade < student.AverageGrade)
+                {
+                    betterStudents++;
+                }
+            }
+
+            int percent = (100 * betterStudents) / Students.Count;
+
+            if (percent < 20)
+            {
+                return 'A';
+            }
+            if (percent >= 20 && percent < 40)
+            {
+                return 'B';
+            }
+            if (percent >= 40 && percent < 60)
+            {
+                return 'C';
+            }
+            if (percent >= 60 && percent < 80)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
     }
 }
