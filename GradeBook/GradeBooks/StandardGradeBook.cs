d388511e6b85d48b253af51be4f31a113576071f
Filip G <filipgajko18@gmail.com>
2023-04-04 14:53:20 +0200

    Constructor Fix

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -8,9 +8,9 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(string name) : base(name)
+        public StandardGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
-            Type = GradeBookType.Standard;
+            this.Type = GradeBookType.Standard;
         }
     }
 }
